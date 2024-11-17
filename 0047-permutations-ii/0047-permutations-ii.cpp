cc1667feff7eaa41695b37d6e5bcfb8e2d67de46
AdarshSorout <165195626+AdarshSorout@users.noreply.github.com>
2024-11-18 01:06:31 +0530

    Time: 10 ms (34.28%), Space: 15.1 MB (16.8%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,38 +1,34 @@
-
 class Solution {
 public:
+    vector<vector<int>> result;
     int n;
+    
+    void solve(int idx, vector<int>& nums) {
 
-    // Custom hash function for vector<int>
-    struct VectorHash {
-        std::size_t operator()(const std::vector<int>& vec) const {
-            std::size_t hash = 0;
-            for (int num : vec) {
-                hash ^= std::hash<int>()(num) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-            }
-            return hash;
-        }
-    };
-
-    std::unordered_set<std::vector<int>, VectorHash> result;
-
-    void solve(int idx, std::vector<int>& nums) {
-        if (idx == n) {
-            result.insert(nums); // Add the current permutation to the set
+        if(idx == n) {
+            result.push_back(nums);
             return;
         }
-        for (int i = idx; i < n; i++) {
-            std::swap(nums[i], nums[idx]);
-            solve(idx + 1, nums);
-            std::swap(nums[i], nums[idx]); // Backtrack
+        unordered_set<int>s;
+
+        for(int i = idx; i < n; i++) {
+            if(s.find(nums[i]) !=s.end())continue;
+            s.insert(nums[i]);
+            swap(nums[i], nums[idx]);
+            
+            solve(idx+1, nums);
+            
+            swap(nums[i], nums[idx]);
         }
+        
+        
     }
-
-    std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums) {
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
         n = nums.size();
+        
         solve(0, nums);
-
-        // Convert the unordered_set to a vector of vectors
-        return std::vector<std::vector<int>>(result.begin(), result.end());
+        
+        return result;
     }
 };
