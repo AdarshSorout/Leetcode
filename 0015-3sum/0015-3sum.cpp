68dfdc26dc1c23c0d0b71f29f8bef67dfe8a9762
AdarshSorout <165195626+AdarshSorout@users.noreply.github.com>
2025-03-09 20:32:05 +0530

    Time: 43 ms (87.18%), Space: 32.9 MB (24.79%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-vector<vector<int>>result;
-void twoSum(vector<int>& nums,int target,int i,int j){
-    while(i<j){
-        if(nums[i]+nums[j]>target){
-            j--;
-        }
-        else if(nums[i]+nums[j]<target)
-        i++;
-        else{
-            while(i<j && nums[i]==nums[i+1]){
-                i++;
+    vector<vector<int>> res;
+
+    void twoSum(vector<int>& nums, int idx, int target, int n) {
+        int l = idx, r = n - 1;
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+            if (sum == target) {
+                res.push_back({-target, nums[l], nums[r]});
+                l++;
+                r--;
+                while (l < r && nums[l] == nums[l - 1]) l++;
+                while (l < r && nums[r] == nums[r + 1]) r--;
+            } 
+            else if (sum < target) {
+                l++;
+            } 
+            else {
+                r--;
             }
-            while(i<j && nums[j]==nums[j-1]){
-                j--;
-            }
-            result.push_back({-target,nums[i],nums[j]});
-            i++;
-            j--;
         }
     }
-}
+
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        if(n<3){
-            return {};
+        res.clear();
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i];
+            int idx = i + 1;
+            twoSum(nums, idx, target, n);
         }
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            int n1=nums[i];
-            int target=-n1;
-            twoSum(nums,target,i+1,n-1);
-        }
-        return result;
+        return res;
     }
 };
