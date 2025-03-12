e52f90eef751d654685d5fbc44935ee7846446e0
AdarshSorout <165195626+AdarshSorout@users.noreply.github.com>
2025-03-12 22:16:50 +0530

    Time: 3 ms (56.17%), Space: 14.1 MB (54.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int> ans;
-    
-    void backtrack(vector<int>& candidates, int target, int start, int sum) {
-        if (sum == target) {
-            result.push_back(ans);
-            return;
-        }
-        
-        if (sum > target) {
-            return;
-        }
+vector<vector<int>>res;
+vector<int>ans;
 
-        for (int i = start; i < candidates.size(); i++) {
-            ans.push_back(candidates[i]);
-            backtrack(candidates, target, i, sum + candidates[i]);
-            ans.pop_back();
-        }
+void backtrack(vector<int>& candidates, int target, int start,int sum){
+    if(sum==target){
+        res.push_back(ans);
+        return ;
     }
-    
+    if(sum>target){
+        return;
+    }
+    for(int i=start;i<candidates.size();i++){
+        ans.push_back(candidates[i]);
+        backtrack(candidates,target,i,sum+candidates[i]);
+        ans.pop_back();
+    }
+}
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        backtrack(candidates, target, 0, 0);
-        return result;
+        backtrack(candidates,target,0,0);
+        return res;
     }
 };
