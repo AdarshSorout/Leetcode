7c79439169b2c8c1cdb82d1cf5be27a94db0f77f
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-04-05 11:43:17 +0530

    Time: 4 ms (6.22%), Space: 18.7 MB (10.04%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    void flood(vector<vector<int>>& image, int sr, int sc, int temp, int color) {
-        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != temp) {
-            return;
-        }
-        image[sr][sc] = color;
-        for (auto& dir : directions) {
-            flood(image, sr + dir[0], sc + dir[1], temp, color);
-        }
-    }
+vector<vector<int>>directions{{0,1},{1,0},{0,-1},{-1,0}};
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) {
-            return image;
-        }
-        int temp = image[sr][sc];
-        flood(image, sr, sc, temp, color);
+        int n=image.size();
+        int m=image[0].size();
+        int temp=image[sr][sc];
+       
+       if(image[sr][sc]==color){
         return image;
+       }
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        image[sr][sc]=color;
+       
+       while(!q.empty()){
+        auto curr=q.front();
+        q.pop();
+
+        int i=curr.first;
+        int j=curr.second;
+
+        for(auto &dir:directions){
+            int new_i=i+dir[0];
+            int new_j=j+dir[1];
+
+            if(new_i>=0 && new_j>=0 && new_i<n && new_j<m && 
+            image[new_i][new_j] == temp){
+                image[new_i][new_j]=color;
+                q.push({new_i,new_j});
+            }
+        }
+
+       }
+       return image;
     }
 };
