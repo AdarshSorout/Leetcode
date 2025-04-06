3ec784bd77d934e7bd5413cccdc64e16ec6eb489
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-04-06 13:40:28 +0530

    Time: 14 ms (77.91%), Space: 39.1 MB (15.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-       vector<vector<int>>directions{{0,1},{0,-1},{-1,0},{1,0}};
-        vector<vector<int>>res(m,vector<int>(n,-1));
-        queue<pair<int,int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+         int n=mat.size();
+         int m=mat[0].size();
+         vector<vector<int>>vis(n,vector<int>(m,0));
+          vector<vector<int>>dist(n,vector<int>(m,0));
+          queue<pair<pair<int,int>,int>>q;
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    res[i][j]=mat[i][j];
-                  q.push({i,j});
+                   q.push({{i, j}, 0});
+                    vis[i][j]=1;
+
+                }
+                else{
+                    vis[i][j]=0;
                 }
             }
-        }
-        while(!q.empty()){
-             pair<int,int>p=q.front();
-             q.pop();
-            int i=p.first;
-            int j=p.second;
-           
-             for(auto&dir:directions){
-                int new_i=i+dir[0];
-                int new_j=j+dir[1];
-                if(new_i>=0 && new_i<m && new_j<n && new_j>=0){
-                    if(res[new_i][new_j]==-1){
-                        res[new_i][new_j]=res[i][j]+1;
-                        q.push({new_i,new_j});
-                    }
+          }
+
+          int drow[]={-1,0,1,0};
+          int dcol[]={0,1,0,-1};
+
+          while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+
+            q.pop();
+            dist[row][col]=steps;
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow, ncol}, steps + 1});
                 }
-             }
-        }  return res;
-      }
+            }
+          }
+          return dist;
+    }
 };
