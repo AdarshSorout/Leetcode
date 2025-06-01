098be6a98b863d425bcccc425735e933453b2de4
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-01 12:45:59 +0530

    Time: 30 ms (38.47%), Space: 23.6 MB (85.22%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        vector<vector<int>> directions{{1, 1}, {1, 0}, {0, 1}, {0, -1}, {-1, 0}, {-1, -1}, {1, -1}, {-1, 1}};
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if (m == 0 || n == 0 || grid[0][0] != 0) {
+        int n = grid.size();
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
-        }
 
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        int level = 0;
+        priority_queue< pair<int, pair<int,int>>, 
+                        vector<pair<int, pair<int,int>>>, 
+                        greater<pair<int, pair<int,int>>> > q;
 
-        auto isSafe = [&](int x, int y) {
-            return (x >= 0 && x < m && y >= 0 && y < n);
-        };
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        dist[0][0] = 1;  
+        q.push({1, {0, 0}});
+     
+        int dr[] = {-1, -1, 0, +1, +1, +1, 0, -1};
+        int dc[] = {0, +1, +1, +1, 0, -1, -1, -1};
 
         while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                auto curr = q.front();
-                q.pop();
-                int x = curr.first;
-                int y = curr.second;
+            auto it = q.top(); q.pop();
+            int dis = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
 
-                if (x == m - 1 && y == n - 1) {
-                    return level + 1;
-                }
+            if (r == n - 1 && c == n - 1)
+                return dis;
 
-                for (auto dir : directions) {
-                    int new_x = x + dir[0];
-                    int new_y = y + dir[1];
+            for (int i = 0; i < 8; i++) {
+                int newr = r + dr[i];
+                int newc = c + dc[i];
 
-                    if (isSafe(new_x, new_y) && grid[new_x][new_y] == 0) {
-                        q.push({new_x, new_y});
-                        grid[new_x][new_y] = 1;
-                    }
+                if (newr >= 0 && newr < n && newc >= 0 && newc < n &&
+                    grid[newr][newc] == 0 && dis + 1 < dist[newr][newc]) {
+                    
+                    dist[newr][newc] = dis + 1;
+                    q.push({dist[newr][newc], {newr, newc}});
                 }
             }
-            level++;
         }
+
         return -1;
     }
 };
