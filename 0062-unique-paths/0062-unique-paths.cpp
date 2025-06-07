2e63bf762ef835a5b5a1003154f54bd90186b19e
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-07 09:41:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (75.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -18,22 +18,25 @@ int f(int i,int j, vector<vector<int>>&dp){
  
 }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<int>prev(n,0);
        for(int i=0;i<m;i++){
+        vector<int>cur(n,0);
         for(int j=0;j<n;j++){
             if(i==0 && j==0){
-                dp[i][j]=1;
+               cur[j]=1;
             }
             else{
                 int up=0;
                 int left=0;
-                if(i>0)up=dp[i-1][j];
-                if(j>0)left=dp[i][j-1];
-                dp[i][j]=left+up;
+                if(i>0)up=prev[j];
+                if(j>0)left=cur[j-1];
+               cur[j] = up + left;
 
             }
+
         }
+        prev=cur;
        }
-       return dp[m-1][n-1];
+       return prev[n-1];
     }
 };
