f764b87fabc1853e984517cbf5a58e12cda0346e
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-07 10:55:24 +0530

    Time: 0 ms (100%), Space: 11.7 MB (79.38%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -17,24 +17,28 @@ int f(int i,int j,vector<vector<int>>&mat,vector<vector<int>>&dp){
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
         int mod=2*10^9;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<int>prev(m,0);
+
 
         for(int i=0;i<n;i++){
+             vector<int>cur(m,0);
             for(int j=0;j<m;j++){
+               
                 if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
+                    cur[j]=0;
                 }
                 else if(i==0 && j==0){
-                    dp[i][j]=1;
+                    cur[j]=1;
                 }
                 else{
                     int up=0,left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    if(i>0) up=prev[j];
+                    if(j>0) left=cur[j-1];
+                    cur[j]=up+left;
                 }
             }
+            prev=cur;
         }
-     return dp[n-1][m-1];
+     return prev[m-1];
     }
 };
