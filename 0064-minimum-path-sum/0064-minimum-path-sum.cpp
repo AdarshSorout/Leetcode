ef6858c3a74949970e31d4148d247e74ca04d20c
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-07 12:42:20 +0530

    Time: 3 ms (29.89%), Space: 16.3 MB (58.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -14,6 +14,22 @@ if(dp[i][j]!=-1)return dp[i][j];
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n - 1, m - 1, grid,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)dp[i][j]=grid[i][j];
+                else{
+                    int up=grid[i][j];
+                    if(i>0)up+=dp[i-1][j];
+                    else up=INT_MAX;
+
+                    int left=grid[i][j];
+                    if(j>0)left+=dp[i][j-1];
+                    else left=INT_MAX;
+
+                    dp[i][j]=min(left,up);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
