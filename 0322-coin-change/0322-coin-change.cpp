f99adc443ce3bb1072bd87f838a0a0475c605527
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-09 16:07:30 +0530

    Time: 51 ms (25.23%), Space: 48.8 MB (39.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-int helper(vector<int>&coins,int amount,vector<int>&dp){
-     if(amount==0){
-            return 0;
+    int f(int idx, int t, vector<int>& coins) {
+        if (idx == 0) {
+            if (t % coins[0] == 0) return t / coins[0];
+            return 1e9; 
         }
-        if(amount<0){
-            return INT_MAX;
-        }
-        if(dp[amount] != -1)
-        return dp[amount];
 
-        int minCoins=INT_MAX;
-    
-            for(int i=0;i<coins.size();i++){
-           int res=helper(coins,amount-coins[i],dp);
-           if(res!=INT_MAX){
-            minCoins=min(minCoins,1+res);
-           }
-            }
-            return dp[amount]=minCoins;
-        
-}
+        int notTake = f(idx - 1, t, coins);
+        int take = 1e9;
+        if (coins[idx] <= t) take = 1 + f(idx, t - coins[idx], coins);
+
+        return min(take, notTake); 
+    }
+
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<int>dp(amount+1,-1);
-       int result=helper(coins,amount,dp);
-      if(result==INT_MAX){
-        return -1;
-      }
-return result;
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
 
+        for(int T=0;T<=amount;T++){
+            if(T%coins[0]==0)dp[0][T]=T/coins[0];
+            else dp[0][T]=1e9;
+        }
+
+
+for(int i=1;i<n;i++){
+    for(int T=0;T<=amount;T++){
+int notTake=0+dp[i-1][T];
+int take=1e9;
+if(coins[i]<=T){
+    take=1+dp[i][T-coins[i]];
+
+}
+dp[i][T]=min(take,notTake);
+    }
+}
+int ans=dp[n-1][amount];
+if(ans>=1e9)return -1;
+return ans;
     }
 };
