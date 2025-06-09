ff2c6dfbdafcc46da648793f6b36e72c39f613b0
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-09 22:58:25 +0530

    Time: 23 ms (44.94%), Space: 46.2 MB (12.3%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -11,8 +11,25 @@ if(dp[idx][t]!=-1)return dp[idx][t];
     return dp[idx][t]=take+notTake;
 }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-       vector<vector<long>>dp(n,vector<long>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
+    int n = coins.size();
+    using ull = unsigned long long;
+    vector<vector<ull>> dp(n, vector<ull>(amount + 1, 0));
+
+    for (int T = 0; T <= amount; T++) {
+        if (T % coins[0] == 0) dp[0][T] = 1;
     }
+
+    for (int idx = 1; idx < n; idx++) {
+        for (int T = 0; T <= amount; T++) {
+            ull notTake = dp[idx - 1][T];
+            ull take = 0;
+            if (coins[idx] <= T)
+                take = dp[idx][T - coins[idx]];
+            dp[idx][T] = take + notTake;
+        }
+    }
+
+    return dp[n - 1][amount];
+}
+
 };
