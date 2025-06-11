5623539c9267ab38b78ae8abb50ec7212c6f7612
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-11 07:27:15 +0530

    Time: 34 ms (38.31%), Space: 27 MB (63.95%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -15,7 +15,7 @@ public:
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         return f(n-1, m-1, s, t, dp);
     }
 };
