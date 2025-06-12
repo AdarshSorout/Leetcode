784b28c33ce69d454a2bd79d8952f236cc47d36b
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-12 16:43:48 +0530

    Time: 51 ms (69.06%), Space: 79.4 MB (81.67%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,27 +17,26 @@ public:
         return dp[idx][buy][cap] = profit;
     }
 
+   
     int maxProfit(vector<int>& prices) {
-       int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-       for (int idx = n - 1; idx >= 0; idx--) {
+        int n = prices.size();
+        vector<vector<int>> ahead(2, vector<int>(3, 0)), cur(2, vector<int>(3, 0));
+
+        for (int idx = n - 1; idx >= 0; idx--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= 2; cap++) {
                     if (buy) {
-                        dp[idx][buy][cap] = max(
-                            -prices[idx] + dp[idx + 1][0][cap],  
-                            dp[idx + 1][1][cap]                  
-                        );
+                        cur[buy][cap] = max(-prices[idx] + ahead[0][cap], ahead[1][cap]);
                     } else {
-                        dp[idx][buy][cap] = max(
-                            prices[idx] + dp[idx + 1][1][cap - 1],  
-                            dp[idx + 1][0][cap]                    
-                        );
+                        cur[buy][cap] = max(prices[idx] + ahead[1][cap - 1], ahead[0][cap]);
                     }
                 }
             }
+            ahead = cur;
         }
 
-        return dp[0][1][2];
+        return ahead[1][2];
     }
+
+
 };
