9b39482534b2cb41a8d68b2b983b205b4f81a2a7
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-12 15:42:51 +0530

    Time: 3 ms (36.29%), Space: 19.8 MB (24.89%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-int f(int idx,int buy,vector<int>& prices,int n, vector<vector<int>>&dp){
-    if(idx==n)return 0;
+int f(int i,int buy,vector<int>& prices,int n, vector<vector<int>>&dp){
+    if(i==n)return 0;
     long profit=0;
-    if(dp[idx][buy]!=-1)return dp[idx][buy];
+    if(dp[i][buy]!=-1)return dp[i][buy];
     if(buy){
-        profit=max(-prices[idx]+f(idx+1,0,prices,n,dp),
-                    0+f(idx+1,1,prices,n,dp));
+        profit=max(-prices[i]+f(i+1,0,prices,n,dp),
+                    0+f(i+1,1,prices,n,dp));
 
     }
     else{
-        profit=max(prices[idx]+f(idx+1,1,prices,n,dp),
-                0+ f(idx+1,0,prices,n,dp));
+        profit=max(prices[i]+f(i+1,1,prices,n,dp),
+                0+ f(i+1,0,prices,n,dp));
     }
-    return dp[idx][buy]=profit;
+    return dp[i][buy]=profit;
 }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return f(0,1,prices,n,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+          for(int buy=0;buy<=1;buy++){
+            long profit=0;
+                if(buy){
+        profit=max(-prices[i]+dp[i+1][0],
+                    0+dp[i+1][1]);
+
+    }
+    else{
+        profit=max(prices[i]+dp[i+1][1],
+                0+ dp[i+1][0]);
+    }
+       dp[i][buy]=profit;
+          }
+       
+        }
+        return dp[0][1];
     }
 };
