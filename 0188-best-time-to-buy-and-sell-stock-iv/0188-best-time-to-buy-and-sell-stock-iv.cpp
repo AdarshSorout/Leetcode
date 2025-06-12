7b1a96c06ebbee7e8c697f2917f056b35c31d0e2
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-12 16:49:39 +0530

    Time: 4 ms (73.75%), Space: 18.2 MB (35.97%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-int f(int idx, int buy, int cap, vector<int>& prices, int n, vector<vector<vector<int>>>& dp) {
-        if (cap == 0 || idx == n) return 0;
-        
-        if (dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
-
-        long profit = 0;
-        if (buy) {
-            profit = max(-prices[idx] + f(idx + 1, 0, cap, prices, n, dp),
-                         0 + f(idx + 1, 1, cap, prices, n, dp));
-        } else {
-            profit = max(prices[idx] + f(idx + 1, 1, cap - 1, prices, n, dp),
-                         0 + f(idx + 1, 0, cap, prices, n, dp));
-        }
-
-        return dp[idx][buy][cap] = profit;
-    }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return f(0,1,k,prices,n,dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy) {
+                        dp[idx][buy][cap] = max(-prices[idx] + dp[idx + 1][0][cap],
+                                                dp[idx + 1][1][cap]);
+                    } else {
+                        dp[idx][buy][cap] = max(prices[idx] + dp[idx + 1][1][cap - 1],
+                                                dp[idx + 1][0][cap]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][k];
     }
 };
