2d926d5748c7f555f26122e25ed525289f6ecc68
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-13 23:09:23 +0530

    Time: 21 ms (68.62%), Space: 13.9 MB (77.01%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -11,11 +11,22 @@ if(dp[i][j]!=-1)return dp[i][j];
     return dp[i][j]=mini;
 }
     int minCost(int n, vector<int>& cuts) {
-        int c=cuts.size();
+        int c = cuts.size();
         cuts.push_back(n);
-        cuts.insert(cuts.begin(),0);
-        sort(cuts.begin(),cuts.end());
-        vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
-        return f(1,c,cuts,dp);
+        cuts.insert(cuts.begin(), 0);
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
+        for (int i = c; i >= 1; i--) {
+            for (int j = 1; j <= c; j++) {
+                if (i > j) continue;
+                int mini = INT_MAX;
+                for (int idx = i; idx <= j; idx++) {
+                    int cost = cuts[j + 1] - cuts[i - 1] + dp[i][idx - 1] + dp[idx + 1][j];
+                    mini = min(mini, cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
     }
 };
