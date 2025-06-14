2eb7f13afc824ce14e8f71fe6826e778d8d77437
Adarsh Sorout <165195626+AdarshSorout@users.noreply.github.com>
2025-06-14 10:10:14 +0530

    Time: 1502 ms (43.5%), Space: 9.1 MB (93.7%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool isPalindrome(int i, int j, string &s) {
+    bool isPal(int i, int j, string &s) {
         while (i < j) {
             if (s[i] != s[j]) return false;
             i++;
@@ -10,26 +10,23 @@ public:
         return true;
     }
 
-    int f(int i, int n, string &str, vector<int> &dp) {
-        if (i == n) return 0;
-
-        if (dp[i] != -1) return dp[i];
-
-        int minCost = INT_MAX;
-
-        for (int j = i; j < n; j++) {
-            if (isPalindrome(i, j, str)) {
-                int cost = 1 + f(j + 1, n, str, dp);
-                minCost = min(minCost, cost);
-            }
-        }
-
-        return dp[i] = minCost;
-    }
-
+  
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, n, s, dp) - 1;
+      
+         vector<int> dp(n + 1, 0); 
+        for (int i = n - 1; i >= 0; --i) {
+            int minCost = INT_MAX;
+            for (int j = i; j < n; ++j) {
+                if (isPal(i,j,s)) {
+                    int cost = 1 + dp[j + 1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+    
+        return dp[0] - 1;  
+    
     }
 };
